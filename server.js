927dc8aa84ee087209adc1ee26ebec6814af860f
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-10 20:14:50 +0200

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 // initialise app varibale with express
 const app = express();
 
+// connect db
+connectDB();
+
+// init middleware for body parser; allows to get data in req.body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // looks for an env variable called PORT;
 const PORT = process.env.PORT || 5000;
