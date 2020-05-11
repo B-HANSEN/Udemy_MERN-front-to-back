d719081c2a61d4e9740dc502da77e8f7c968ba1a
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-11 20:39:12 +0200

    setup profile model and profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,112 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route   GET api/profile/me   // based on user ID that is in the token
+// @desc    Get current user's profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+	try {
+		// set user to the userID that comes in with the token
+		// use populate to add stuff to the query (name, avatar are in the User model, not the Profile model)
+		const profile = await Profile.findOne({
+			user: req.user.id,
+		}).populate('user', ['name', 'avatar']);
+
+		if (!profile) {
+			return res
+				.status(400)
+				.json({ msg: 'There is no profile for this user.' });
+		}
+
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error.');
+	}
+});
+
+// @route   POST api/profile
+// @desc    Create or update a user's profile
+// @access  Private
+// require auth and validation middleware, so pass in as []
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status is required.').not().isEmpty(),
+			check('skills', 'Skills is required.').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			company,
+			location,
+			website,
+			bio,
+			skills,
+			status,
+			githubusername,
+			youtube,
+			twitter,
+			instagram,
+			linkedin,
+			facebook,
+		} = req.body;
+
+		// build profile object, initialise first
+		const profileFields = {};
+		profileFields.user = req.user.id;
+		if (company) profileFields.company = company;
+		if (website) profileFields.website = website;
+		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (githubusername) profileFields.githubusername = githubusername;
+		// turn skills (comma-separated list) into an array with split(',')-method
+		// trim()-method eliminates leading and trailing spaces
+		if (skills) {
+			profileFields.skills = skills.split(',').map(skill => skill.trim());
+		}
+
+		// build social object, initialise first
+		profileFields.social = {};
+		if (youtube) profileFields.social.youtube = youtube;
+		if (twitter) profileFields.social.twitter = twitter;
+		if (facebook) profileFields.social.facebook = facebook;
+		if (linkedin) profileFields.social.linkedin = linkedin;
+		if (instagram) profileFields.social.instagram = instagram;
+
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
+
+			// if there is a profile, update it
+			if (profile) {
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+				return res.json(profile);
+			}
+			// else create new profile
+			profile = new Profile(profileFields);
+			await profile.save();
+			res.json(profile);
+		} catch (err) {
+			console.error(err.messafge);
+			res.status(500).send('Server Error.');
+		}
+	}
+);
 
 module.exports = router;
