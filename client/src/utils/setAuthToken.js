092c14ca04eab9ea603612b49287858bbf4658b2
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-16 19:11:44 +0200

    created util function to check for token, dispatch loadUser for every componentDidMount, setup register and loadUser actions and reducers

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,16 @@
+// function that takes in the token
+// if token there, add it to headers; if not, delete from headers
+
+import axios from 'axios';
+
+// token sent with every request
+// other than always identifying which request to send with
+const setAuthToken = token => {
+	if (token) {
+		axios.defaults.headers.common['x-auth-token'] = token;
+	} else {
+		delete axios.defaults.headers.common['x-auth-token'];
+	}
+};
+
+export default setAuthToken;
