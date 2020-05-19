1edfbb6b85f00ef72724e99b0f59d39483468ec7
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-19 22:57:17 +0200

    solved loadUser action type bug, setup spinner component, added clear profile to ensure cleanup when logout, conditional rendering in dashboard, setup create profile component

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
 	LOGIN_SUCCESS,
 	LOGIN_FAIL,
 	LOGOUT,
+	CLEAR_PROFILE,
 } from './types';
 
 // Load user
@@ -42,7 +43,7 @@ export const register = ({ name, email, password }) => async dispatch => {
 			type: REGISTER_SUCCESS,
 			payload: res.data,
 		});
-		dispatch(loadUser);
+		dispatch(loadUser());
 	} catch (err) {
 		const errors = err.response.data.errors;
 		if (errors) {
@@ -70,7 +71,7 @@ export const login = (email, password) => async dispatch => {
 			type: LOGIN_SUCCESS,
 			payload: res.data,
 		});
-		dispatch(loadUser);
+		dispatch(loadUser());
 	} catch (err) {
 		const errors = err.response.data.errors;
 		if (errors) {
@@ -84,5 +85,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout user & clear profile
 export const logout = () => dispatch => {
+	dispatch({ type: CLEAR_PROFILE });
 	dispatch({ type: LOGOUT });
 };
