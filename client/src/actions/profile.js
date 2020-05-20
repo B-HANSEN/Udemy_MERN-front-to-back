24726b8213b7edc5f30fe35a726227b8336c3a92
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-20 22:31:13 +0200

    create and edit a profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -23,3 +23,47 @@ export const getCurrentProfile = () => async dispatch => {
 		});
 	}
 };
+
+// create or update profile
+// history object (with method push) required to redirect to a client-side route
+// editing or creating new profile --> edit (avoid creating a separate function)
+export const createProfile = (
+	formData,
+	history,
+	edit = false
+) => async dispatch => {
+	try {
+		// to send data, create a config object
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		const res = await axios.post('/api/profile', formData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			// this route returns all profile data
+			payload: res.data,
+		});
+		dispatch(
+			setAlert(edit ? 'Profile updated.' : 'Profile created.', 'success')
+		);
+		if (!edit) {
+			history.push('/dashboard');
+		}
+	} catch (err) {
+		const errors = err.response.data.errors;
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+		}
+
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status,
+			},
+		});
+	}
+};
