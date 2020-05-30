755f4a55382e8e792132db621d1f20eb625e25ad
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-30 14:49:47 +0200

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // initialise app varibale with express
 const app = express();
@@ -10,13 +11,25 @@ connectDB();
 // init middleware for body parser; allows to get data in req.body
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
+// must be removed prior heroku build process:
+// app.get('/', (req, res) => res.send('API running'));
 
 // define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+	// serve index.html file
+	app.get('*', (req, res) => {
+		// current directory, go into client folder, into build folder, load index.html
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 // looks for an env variable called PORT;
 const PORT = process.env.PORT || 5000;
