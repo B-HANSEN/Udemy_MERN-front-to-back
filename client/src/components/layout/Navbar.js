bcce9a5bbf5dd689220fd3a261d1fdfcd92fca73
B-HANSEN <b.hansen.ubiqum@gmail.com>
2020-05-31 19:11:17 +0200

    replaced githubToken

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,10 +20,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 				</Link>
 			</li>
 			<li>
-				<a onClick={logout} href='#!'>
+				<Link to='/' onClick={logout}>
 					<i className='fas fa-sign-out-alt' />{' '}
 					<span className='hide-sm'>Logout</span>
-				</a>
+				</Link>
 			</li>
 		</ul>
 	);
